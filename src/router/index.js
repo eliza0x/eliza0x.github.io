4ab7448beda271987cd52af01c962b602214602d
eliza0x <me@eliza.link>
2018-07-05 21:03:27 +0900

    update: SPA部分以下もGoogle Analyticsで追えるように変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueAnalytics from 'vue-analytics'
+
 import RootPage from '@/components/RootPage'
 import PostPage from '@/components/PostPage'
 import PostListPage from '@/components/PostListPage'
@@ -7,6 +9,10 @@ import SlideListPage from '@/components/SlideListPage'
 import TagPageListPage from '@/components/TagPageListPage'
 
 Vue.use(Router)
+Vue.use(VueAnalytics, {
+  id: 'UA-110094040-1',
+  Router
+})
 
 export default new Router({
   routes: [
